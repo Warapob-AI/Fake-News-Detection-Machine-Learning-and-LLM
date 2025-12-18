09ce3b289cb0e669c90dc2a2239ba45e8b58fad9
Warapob-AI <paintseason158@gmail.com>
2025-12-18 16:06:28 +0700

    update

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -224,7 +224,7 @@ const DetectText = React.forwardRef((props, ref) => {
       if (inputType === 'link') {
         webhookUrl = "https://paintaisystemn8n.ggff.net/webhook/ai-check-linknews-thai";
       } else {
-        webhookUrl = "http://localhost:5678/webhook/local-test-speed";
+        webhookUrl = "http://paintaisystemn8n.ggff.net/webhook/ai-check-textnews-thai";
       }
 
       const payload = { taskUser: newsText };
